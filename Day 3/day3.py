0bd3d4da74bf85639fb8c274c981b0fd927b6aaa
Martijn <martijnhielema@gmail.com>
2020-12-03 12:15:07 +0100

    Day 3, parts 1 and 2

---
+++ Day 3/day3.py
@@ -0,0 +1,24 @@
+with open('../input/day3.txt', 'r') as f:
+    layout = [line.strip() for line in f.readlines()]
+
+
+down_steps = 1
+right_steps = 3
+slopes = ((1,1), (3,1), (5,1), (7,1), (1,2))
+answer = 1
+for right_steps, down_steps in slopes:
+    trees = 0
+    down = 0
+    right = 0
+    while down < len(layout):
+        right = right % len(layout[0])
+        if layout[down][right] == '#':
+            trees += 1
+
+        down += down_steps
+        right += right_steps
+
+    print(f'Right: {right_steps}, Down: {down_steps}, Trees: {trees}')
+    answer *= trees
+
+print(f'Multiplication: {answer}')
