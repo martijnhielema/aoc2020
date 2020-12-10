d8d134e46fbec19fa1420220c3d26957913cd255
Martijn Hielema <martijn.hielema@sogeti.com>
2020-12-10 14:37:25 +0100

    Day 10 part 2

--- Day 10/day10.py
+++ Day 10/day10.py
@@ -1,4 +1,8 @@
-with open('../input/dad.txt', 'r') as f:
+from functools import reduce
+
+import networkx as nx
+
+with open('../input/day10.txt', 'r') as f:
     adapters = [int(x.strip()) for x in f.readlines()]
 
 adapters.append(0)
@@ -8,6 +12,47 @@ differences = {'1': 0, '2': 0, '3': 1}
 for i in range(1, len(adapters)):
     diff = adapters[i] - adapters[i-1]
     differences[str(diff)] += 1
+# print(differences)
+# print(differences['1'] * differences['3'])
 
-print(differences)
-print(differences['1'] * differences['3'])
+adapters.append(max(adapters)+3)
+
+# Build indices where to split groups
+indices = [0]
+for i in range(0, len(adapters) - 1):
+    if adapters[i+1] - adapters[i] == 3:
+        indices.append(i+1)
+
+groups = []
+
+# Build groups
+for i in range(0, len(indices) - 1):
+    ifrom = indices[i]
+    to = indices[i + 1]
+
+    groups.append(adapters[ifrom:to])
+
+# Build DAG for each group and calculate number of possible paths
+answerlist = []
+for group in groups:
+    connected_pairs = []
+    for i in range(0, len(group) - 1):
+        start = group[i]
+        for j in range(i + 1, len(group)):
+            if group[j] - start <= 3:
+                connected_pairs.append((str(start), str(group[j])))
+            else:
+                break
+
+    if len(group) > 1:
+        graph = nx.DiGraph()
+        graph.add_edges_from(connected_pairs)
+
+        paths = tuple(nx.all_simple_paths(graph, str(min(group)), str(max(group))))
+        numpaths = len(paths)
+    else:
+        numpaths = 1
+    answerlist.append(numpaths)
+
+print(answerlist)
+print(reduce(lambda x, y: x*y, answerlist))
